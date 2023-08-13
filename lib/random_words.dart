6542b284c206e46470b350cae986f0da2e2c2606
Anurag Pramanik <p.anurag@outlook.com>
2023-08-13 15:45:36 +0530

    add favorite logic

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -8,6 +8,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
 
   Widget _buildList() {
     return ListView.builder(
@@ -18,18 +19,54 @@ class RandomWordsState extends State<RandomWords> {
           if (index >= _randomWordPairs.length) {
             _randomWordPairs.addAll(generateWordPairs().take(10));
           }
-
           return _buildRow(_randomWordPairs[index]);
         });
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: TextStyle(fontSize: 18),
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedWordPairs.remove(pair);
+          } else {
+            _savedWordPairs.add(pair);
+          }
+        });
+      },
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: TextStyle(fontSize: 18),
+          ),
+        );
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple[900],
+          title: Text('Saved WordPairs'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 
   Widget build(BuildContext context) {
@@ -37,6 +74,12 @@ class RandomWordsState extends State<RandomWords> {
         appBar: AppBar(
           backgroundColor: Colors.purple[900],
           title: const Text('WordPair Generator'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushSaved,
+            )
+          ],
         ),
         body: _buildList());
   }
